89c1a1412a7bf362581653c1ee50147033136ecd
suzuxander <suzuxander@gmail.com>
2022-06-13 00:23:45 +0900

    Add script

---
+++ script/init.sh
@@ -0,0 +1,12 @@
+npm i
+
+echo ACCOUNT_ID= >> .env
+echo REGION= >> .env
+echo CLIENT_ID= >> .env
+echo CLIENT_SECRET= >> .env
+echo BUCKET= >> .env
+echo REDIRECT_URI=dummy >> .env
+
+cd cdk
+npm i
+
